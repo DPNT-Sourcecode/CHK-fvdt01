434b156fbf8a1f1495fdd0973f108f0d54a398f3
sbx_user1051 <sbx_user1051@169.254.84.245>
2020-02-05 19:59:16 +0000

    Wed Feb 05 19:59:16 UTC 2020

---
+++ BeFaster.Domain/Cqrs/Results/SumCommandResult.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using BeFaster.Core.Cqrs;
+
+namespace BeFaster.Core
+{
+    public class SumCommandResult : IResult
+    {
+        public SumCommandResult()
+        {
+
+        }
+        public SumCommandResult(IDictionary<string, string> errors)
+        {
+            Errors = errors;
+            HasErrors = errors.Count > 0 ? true : false;
+        }
+
+        public IDictionary<string, string> Errors { get; set; }
+        public bool HasErrors { get; set; }
+    }
+}
