d0159d37d29fe40e7afa4a37d8d29106da753242
sbx_user1051 <sbx_user1051@169.254.84.245>
2020-02-05 19:50:15 +0000

    Wed Feb 05 19:50:15 UTC 2020

---
+++ BeFaster.Core/Cqrs/ICommandHandler.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace BeFaster.Core.Cqrs.CommandHandlers
+{
+    public interface ICommandHandler<in TParameter, TResult>
+                 where TParameter : ICommand
+                 where TResult : IResult
+    {
+        Task<TResult> HandleAsync(TParameter command);
+    }
+}
