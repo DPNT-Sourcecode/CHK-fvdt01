150b8c7486fda8e80c28d65c912193717b4f99e9
sbx_user1051 <sbx_user1051@169.254.61.229>
2020-02-08 08:10:20 +0000

    Sat Feb 08 08:10:20 UTC 2020

--- src/BeFaster.App.Tests/Solutions/HLO/AppTests.cs
+++ src/BeFaster.App.Tests/Solutions/HLO/AppTests.cs
@@ -10,16 +10,25 @@ namespace BeFaster.App.Tests.Solutions.HLO
     public class AppTests
     {
         [Theory]
-        [InlineData("John", "Hello, World!")]
-        public void App_HelloReturnsResult_WhenValidParams(string message, string expected)
+        [InlineData("", "Hello, World!")]
+        public void App_HelloReturnsResult_WhenEmptyString(string message, string expected)
         {
             var result = HelloSolution.Hello(message);
 
             Assert.Equal(expected, result);
-        }      
+        }
 
         [Theory]
-        [InlineData("")]
+        [InlineData("John", "Hello, John!")]
+        public void App_HelloReturnsResult_WhenValidParams(string friendName, string expected)
+        {
+            var result = HelloSolution.Hello(friendName);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(null)]
         public void App_HelloThrowsException_WhenMessageEmpty(string message)
         {
             //act
@@ -30,3 +39,4 @@ namespace BeFaster.App.Tests.Solutions.HLO
         }
     }
 }
+
